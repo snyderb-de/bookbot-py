3ca554bcb7de6f3fa52a9840c6d954a532ad6777
Bryan Snyder <snyderb.de@gmail.com>
2024-09-08 01:42:38 -0400

    added character counting

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ def main():
         word_count = len(words)
         print(f"Word Count: {word_count}")
 
+    char_count = count_characters(file_contents)
+    print("Character Count", char_count)
+
+
+def count_characters(text):
+    text = text.lower()
+    char_count = {}
+
+    for char in text:
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+
+    return char_count
 
 if __name__ == "__main__":
     main()
